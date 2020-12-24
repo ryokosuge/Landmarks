14aa3a97bddafd30f42a55b8920c2eaf412f3732
ryokosuge <ryo.kosuge@gmail.com>
2020-12-24 11:26:16 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ryokosuge on 2020/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
